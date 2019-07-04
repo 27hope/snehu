15b1ff79c3c1f21fb0a6070fc64d6494f0b1113c
27hope <52448010+27hope@users.noreply.github.com>
2019-07-04 16:44:01 +0530

    Add files via upload

--- vowels.py
+++ vowels.py
@@ -3,9 +3,9 @@ str1=str(input())
 vowels=["a","e","i","o","u"]
 if (str1.isalpha())==True:
     if str1 in vowels:
-        print("vowels")
+        print("Vowels")
     else:
-        print("consonant")
+        print("Consonant")
 
 else:
     print("Invalid")
