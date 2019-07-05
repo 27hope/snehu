90e81204c169746bfd9525a7b296106ce8d7d9a1
27hope <52448010+27hope@users.noreply.github.com>
2019-07-05 16:29:59 +0530

    Add files via upload

--- factorial1.py
+++ factorial1.py
@@ -1,6 +1,10 @@
 num1=int(input())
 fact=1
-if num1>1:
+if num1<0:
+    print("no factorial for negative number")
+elif num1==0:
+    print("1")
+else:
     for i in range(1,num1+1):
         fact=fact*i
     print(fact)
